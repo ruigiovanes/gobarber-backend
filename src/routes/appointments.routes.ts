6007cc7eaebfb76b0973c89416df4467cc2f3f95
Giovane Coelho <rui.ecs@interplayers.com.br>
2021-03-28 15:49:33 -0300

    - adjusts on avatar upload and update; - added middlewares for handling exceptions.

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -12,30 +12,22 @@ const appointmentsRouter = Router();
 appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', async (request, response) => {
-  try {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-    const appointments = await appointmentsRepository.find();
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
-    return response.json(appointments);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointments);
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider_id, date } = request.body;
+  const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
+  const parsedDate = parseISO(date);
 
-    const createAppointmentService = new CreateAppointmentService();
+  const createAppointmentService = new CreateAppointmentService();
 
-    const appointment = await createAppointmentService.execute({ date: parsedDate, provider_id });
+  const appointment = await createAppointmentService.execute({ date: parsedDate, provider_id });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
 });
 
 export default appointmentsRouter;
